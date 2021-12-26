341b5d1e961624c07394bde577a62096311136fe
Eslam Mohammed <51241988+xS4yk0x@users.noreply.github.com>
2021-12-26 17:46:30 +0200

    Board done

---
+++ Tic-Tac-Toe/src/Classes/Computer.java
@@ -0,0 +1,11 @@
+
+package Classes;
+
+public class Computer extends Player{
+    
+    public Computer(char letter)
+    {
+        this.letter=letter;
+        this.name = "Computer";
+    }
+}
---
+++ Tic-Tac-Toe/src/Classes/Person.java
@@ -0,0 +1,12 @@
+
+package Classes;
+
+public class Person extends Player{
+    public int score;
+    public Person(char letter, int score, String name)
+    {
+        this.letter = letter;
+        this.score = score;
+        this.name = name;
+    }
+}
